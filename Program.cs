26e41473e75d31122b65a4880418a4f06613d3cc
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-27 21:57:14 -0300

    Programa exibe uma aciiart

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Arte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmhd- .-::-. ddhhNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMhos/+ydNdysmMmyhyo:+somMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN:/omNMMMMNms/NN+yNhoyyo:+NMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd+/hNMNmdddmNMNdmNOD+/ymmmd/`mMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmo/hNmyoydmNMNmddmNNdMmddsosyh++hMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+:oNmo:odNNNdyymNMMNyhMMmyNNNNNNo+mMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh-sNh++dNNNy+/hNMMNmysdMNMhyMmmMMN+-NMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh /MNhdMMMms-/dNNNNho+smMNMmsdMhdMMN-NMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh yMMMMMmdysyNNNNNy:-/+NNNNdsoMNomMMs:yMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMdhhdddddhhhs dMMMMM/++odNNMMd++--sMNNNy/:NNyoMMm`+MMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMsos-:+oooo++/-..yNMMMmyhhs+//ymsss:+NMMMMo/oNNm:NMM`+MMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMNh/:+shhhdddddddddhd+ohNhmoyhhd/.:`-:/smNNNNssyMMMoNMN`+MMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMNd//shhmMMMMMNddhhddmN:+:y+m.-/osy- ````:/+++++syMMNyNMo:sMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMy+/sshNMMMNdhhdmNMMMMMNo/h/`.:--:.:`   `-ohhdmds/sMMMMMy.NMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMN::ssyNMMMmdmNMMMMMMMMMh--oy: `          ./`::hyhMNNMMMN+/hNMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMo /o/yNMMMydMMMMMMMMMMN+ `/+os/s`    `.``  .--//odhhyommsooo+/.`+dhNMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMs:/o:oNNMNomMMMMMMMdyo/::+oyyss:`  `::/::.    `...-ys:sNdhdmdhyyyso:/sMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMM`/o-/mNMNshMMm+sh/.``--:///yoo:+/` `h+ooos:`  .++-hy++NMMMmhhmMMNdhys::hNMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMmh-s:-dNNNm/NMMN:--/:/sso/y/o::/::-+/`:/oss+.    .++/o/.+MMMMMNhymMMMNhyo./MMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMs`s+:oNNNNs/NMNm-  ./yd:syyysssNMMo:.+:``.--::///+.`oo+.`hMMMMMMNdhmMMMdys:oNMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMs hs:mMMNN+/NNNNyo+omMs:MMMMMMMMNm-..:y+//+o..-oydd/os+..:MMMMMMMMNddMMMdss.hNMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMs ds:NMMNM+/NNNmyymMMM:-MMMMMNhho-oysh:://s+`-yNMMMo/+/-m/NMMMMMMMMMddMMMhss.+MMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMs hsomMMMMs/NMNhyhMMMM``osyso/.--..y++s::+ysy/`ohMMhsysyM/NMMMMMMMMMMoMMNmos/+MMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMy.oysdMMMMmomMhhymMMMN   -/-::-`.``/yooos+oy/----:MMMMMMm/NMMMMMMNMMM+NMNNo:y+mMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMNm/hsyMMMMMyhmyyyNMMMd..`//`::/-.-:``oyo++-::  `:-dhhdNm-:MMMMMMMNMMN/NMNNo-s-.NMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMM.hssNMMMMddhyyyNMMMd/oyhs/-o+--+:-.o.   .-o`-  -:-:`yd oMMMMMMNNMNh/NNNNo-+/ NMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMM`+ysdMMMMMdyyyhNNmdhhyhyhyydso+odo+:  `/s+- ./../:/.hm.mMMMMMMNNNm/sNNNN+-++ NMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMM:-hsyMMMMMhyyyhhhyyyyyhyhs+ohysooh+-.:+h/-..-o/+ss-Ndy+MMMMMMMNNNo:mNNMN//o/ NMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMo/hsmMMMMhyyhhhhhdms+/syoo/+hyyshsyyyososyhdhhhhd:o-`mMMMMMMNNMh:hNNMMm:sy-`NMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMo`syyMMMMNmdmNMMMMN..hNMMMy`sysyhyyyshyyyhsohyyyhs `yMMMMMMNNMd+hMMNMNooy+/hMMMMMMM");
+            Console.WriteLine("MMMMMNhdddhhdddhMMN.hsdMMMMMNhMMMMMs-+MMMMMMM:hshysysysyyyy` shyyyd:/MMMMMMMNMhsdMMMMMyoyo:sMMMMMMMM");
+            Console.WriteLine("MMMMMm:ydmNNmds/so+ -hyMMMMMMdNMMMd`yMMMMMMMM:yshsssoysyyh`-o.yhyyydNMMMMMMNNyyNMMMMMysyo`mMMMMMMMMM");
+            Console.WriteLine("MMMMMdsMMMmo++yNN+.h/hsNMMMMMNmMMm:dNMMMMMMMN-yshssysysyd:hmM..hhyyhNMMMMMMmydMMMMMNysyo:dNMMMMMMMMM");
+            Console.WriteLine("MMMMMmoMNm.`:smNmy./:ysMMMMMMNmMd/+MMMMMMMdo`/hyhsssssyyy mMMMs.hhyyhmMMMMdhNMMMMMNysh/:oMMMMMMMMMMM");
+            Console.WriteLine("MMMMMm.mNh/` syyysoo+hhMMMMMMdNy`sMMMMMMmo:+sssshossssyhy mMMMds-hhyyyhmNhmMMMMMMmssy/smMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMm./mNhyossssssoydMMMMMMms/.dNMMMMm+:oossssdsssssssyh/-oMMMM`.NhyyyydNMMMMMMhsyo-:MMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMNo+yNNmmdhhhdmNMMMMMmy:/sNMMMMdy-/:hosssshssssssysyh:+MMMM+-NNdhyyyhMMMMmyyy:/hNMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMms/sdNMMMMMMMMNdy/sssNMMMMMM+-:  yooo/-syssssssyyyy/oNMMMohMMmhyyydMMdyy//sMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMms-/shhhyhs+:yddNMMMNm::/+/+-.``````..+sssyysys+/+`NMMMs/MNNNdyyydmys-ydmmo::hmmmNMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMNmmNN::/NNNmMMMMMMdo:::..` `.::-.`    .++/:.`..-//hmMMo`ddNMMmhyyhd. -/shhhhhy+:/hMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMdo:/-```       ..-:::-------::::/o.:oMho-hNMMMNdhyhy-/mMmoosmMMmy-sdMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMm/:/-``     ```      ` `..........``.:-:sd.:hMMMMMmhhys:/yhs/`-NMMN:ymMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMd.:o.      ````      ``       `    ``  ./--Ns/yNMMMMmy-` .-:--./dNNh/`oMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMmo/yho:.`   `       ```       ``     ``  :/odm`/dNMMMh...-..-:+oyh::.ohMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMd/.os++o+/.         ``         ``     `  `/:yNh:.+hNMNosy..+oo+o+s/` mNNMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMN+::./s//h          `         ` `     :+oy`odMMNN+//hdNMm+ymmhhyohho:-/MMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMs/o/+s+o:.    `            `     `o//+y.-MMMMMy .-ymMMMNMMMNysmd+/oMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs`o+-.yNmdhhhyo:`      `:+:-:/o/o/++oo/:MMMMMNddo..-sdNMMMMMMMMhs`MMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN /ddmmddmdshmhhhhdddNdo::+ssyss+:hMMMMMMMMMMM `sosyyh+oos+s: MMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN -myhdmddm+odhyhmdmNy:sshMMMMMMMMMMMMMMMMMMMM .ssyhh.` +so`:oMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN--myyyyyyd.hhyyyhhhd-/MMMMMMMMMMMMMMMMMMMMMMM..ooys:yddNMMdmMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+myyyyyyd`mhyyyyyd:hNMMMMMMMMMMMMMMMMMMMMMMM `-.:.:dMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+dyyyyyyy-myyyyyho`hMMMMMMMMMMMMMMMMMMMMMMMM`-`-`-MMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+dyyyyyh++dyyyyyd:dNMMMMMMMMMMMMMMMMMMMMMMNm...-omMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+dyyyyym.shhyyhd/-MMMMMMMMMMMMMMMMMMMMMMMMs.-`:`yMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+dyyhyyd hyyyyyh`-MMMMMMMMMMMMMMMMMMMMMMMMs..--smMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm+dyyyyh+ hyyyyhoohmMMMMMMMMMMMMMMMMMMMMMM:--`-.MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.-dyyyyd. hyyyyhys/-sdMMMMMMMMMMMMMMMMMMMM.-`-``MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN`yhyhyhs.`shyyyyyyhy/-omMMMMMMMMMMMMMMMMdo.-.-/sMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNm/dyyyym`:h-/hhyyyyyyhy/-oMMMMMMMMMMMMMMMy -`:`sMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo`hyyyyym /MNy/ohhhyyhhyd.:hhmMMMMMMMMMMMdo....:yMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs/dyyyyhd./MMMM`:hhyyyyyyh//`oMMMMMMMMMMM- -`: MMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM--yhyyyyyh:.-NMMd+`ohyyyyh+::o/:NMMMMMMMMM--`-``MMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`shssssddyoy.NMMMMN`+mdddm/:::++ sMMMMMMNh--.-oMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.ho/:::yysoh+NMMMMN:dsssyod//o//+yMMMMMMh -`: +MMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.ho/:::oso++odNMMMN.:ho/s//ddy/:ssNMMMMNy....hmMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`yoo:ohydssyy.oMMMMN/-ddyhNmmyh+s.:MMMM/`-`: NMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM :ho+///hosdm.oMMMMMho:mhmmysh+:o:`ooo+.-`--:NMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd+:ho+yhddhyh+oMMMMMMm.+dmhsshhsyho+ys:.-.:..dNMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh+-yoo/+yohyh/hMMMMMMN::dhmhoyhNNMhmNs:::sd/.+hMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs/yoyhddhsd/.MMMMMMMyoodhyhdmMMMmNMd+oshMMo-:ysoMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo:yo+/s+yhy`mNMMMMMMd-/ddh+oNMMMMMMMNmMMMmdNyy-MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd-hyhdmhsho`sMMMMMMMN:sdyshNMMMMMMMMMMMMMMNs//MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm`/ys+yodhd oNMMMMMMMy+oyyyyMMMMMMMMMMMMNy-sdMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh:oydhmhydo-:MMMMMMMMh:NdyhMMMMMMMMMMMd++dMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo:yo/y/sdh-.MMMMMMMMy +y+mMMmmMMMMMMs.mMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd:hhdmdyyd:+dMMMMMMms:dNys/+:dMNMMh. mMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh`hhohoydyd.yNMMMMMMM.oms:s+::yhhmMm+:sNMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdo.mMMMMMMMMm`-MMMMMMM`-Mm+/::shhdMMN+o:`MMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN+-yMMMNMMMMMMM+-MMMMMMM`/MMMMNNMMMMMMm+:y+odMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm /NMMMNmo+ooo+y+-dMMMMM-hMMMMMNMMMMMNs/s:/o-:MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.ysNMMMMms/:::/N:dMMMMM:NMMMMMMMNMMMh+:s///o-ymMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/o/ohNMMMMNho:+Nh/yMMdo+MMMMMMMMMMNds::s/m+:o.hMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm:s+//+ydNMMMMmmMN`:MMy yMMMMMMMMMNmhmyoyomNo:o-/MMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm-s//o+:/+hhmMMMMN`:Mh+`NMMMMMNMMMMMMMMMMNNMNs/o:hNMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm-s::+:s::smNMMMNMo/M-.oNMMMMd/MMmddmmmmNNMMMNy++`hMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.s::Mo:yhNMMMmhodh/m--hsydmmo yMs+/////odsymdh:/dNMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm`s::MNoNMMMmy+::+s:-.s-yyyhs-:-oh::::::/h/oysoyNMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd o::NMNMMNdo/:::::/++dossyh/.M+/o::::/+s+syos-hMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm./+:mMMMmsyoo+++//++smddds:+dMos://+ooooyoos:dNMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/s:mNNyhysooooososysdsod/ sMM++sooossoooo+:+MMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+o+hoh+/++++++oosy/+hsNso sMMho:yss++oo/-sdMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMms`ys-/+++ooooo+//`...-:s sMMMNd/////-`+dMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo/-///+/-/hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmsssssssMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+        }
+    }
+}
